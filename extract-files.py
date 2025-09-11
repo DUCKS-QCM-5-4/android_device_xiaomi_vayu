fd21ecf037b5554873892fe8a33b32f17956f58d
duckyduckG <duckyduckg65@gmail.com>
2025-09-11 21:00:58 +0500

    vayu: drop vayu audio hal
    Change-Id: I28e37a72a307105989bc9a9aa4c237af4e02d06c

--- extract-files.py
+++ extract-files.py
@@ -19,14 +19,6 @@ from extract_utils.main import (
 blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/init.batterysecret.rc': blob_fixup()
         .regex_replace('.*seclabel u:r:batterysecret:s0\n', ''),
-    'vendor/lib/libaudioroute_ext.so': blob_fixup()
-        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
-    'vendor/lib/hw/audio.primary.vayu.so': blob_fixup()
-        .binary_regex_replace(
-            b'/vendor/lib/liba2dpoffload.so',
-            b'liba2dpoffload_vayu.so\x00\x00\x00\x00\x00\x00\x00',
-        )
-        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/lib64/hw/camera.qcom.so': blob_fixup()
         .binary_regex_replace(
             b'\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63',
